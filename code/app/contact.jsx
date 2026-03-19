964135d375e46734596be93e7752f07d8afc6cf0
marmag0 <mikolaj.mazur05@gmail.com>
2026-03-19 23:50:46 +0100

    basic interface with routing

---
+++ code/app/contact.jsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { Link } from 'expo-router'
+
+const Contact = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Contact Page</Text>
+
+      <Link href='/' style={styles.link}>Back Home</Link>
+    </View>
+  )
+}
+
+export default Contact
+
+const styles = StyleSheet.create({
+    container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 15
+  },
+  link: {
+    color: '#41adec',
+    marginVertical: 10,
+    borderBottomWidth: 1
+  },
+})
---
+++ code/app/index.jsx
@@ -0,0 +1,86 @@
+// represents the main page (react component)
+// <View> - acts as a <div> component
+
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
+import { Link } from 'expo-router'
+import Logo from '../assets/img/cyberwave-logo.png'
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      
+      <Image source={Logo} style={styles.img} resizeMode='contain'></Image>
+
+      <Text style={styles.title}>The Most Innovative Academic Conference</Text>      
+      <Text style={styles.subTitle}>
+        Dive into the world of cybersecurity, telecommunication and AI!
+      </Text>
+
+      <View>
+
+        <Link href='/about' asChild style={styles.linkBoundry}>
+          <TouchableOpacity style={styles.card}>
+            <Text style={styles.buttonText}>
+              Begin Your Journey
+            </Text>
+          </TouchableOpacity>
+        </Link>
+
+        <Link href='/contact' asChild style={styles.linkBoundry}>
+          <TouchableOpacity style={styles.card}>
+            <Text style={styles.buttonText}>
+              Contact Us
+            </Text>
+          </TouchableOpacity>
+        </Link>
+
+      </View>
+  
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  card: {
+    backgroundColor: '#eee',
+    padding: 20,
+    borderRadius: 25,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 4, height: 4 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3, 
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  img: {
+    marginVertical: 20,
+    height: 50,
+    width: '70%'
+  },
+  linkBoundry: {
+    marginVertical: 10
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 15
+  },
+  subTitle: {
+    width: '70%',
+    textAlign: 'center',
+    marginTop: 10,
+    marginBottom: 10
+  },
+})
