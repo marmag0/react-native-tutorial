010b0b2c740291a61aa59b82993b0bc8ba9b41f9
marmag0 <mikolaj.mazur05@gmail.com>
2026-03-20 15:44:49 +0100

    layout added

---
+++ code/app/_layout.jsx
@@ -0,0 +1,20 @@
+import { Stack } from 'expo-router'
+import { StyleSheet, Text, View } from 'react-native'
+
+
+const RootLayout = () => {
+  return (
+      <Stack screenOptions={{
+        headerStyle: { backgroundColor: '#ddd'},
+        headerTintColor: '#333'
+      }}>
+        <Stack.Screen name='index' options={{ title: 'Home', headerShown: false}}/>
+        <Stack.Screen name='about' options={{ title: 'About'}}/>
+        <Stack.Screen name='contact' options={{ title: 'Contact'}}/>
+      </Stack>
+  )
+}
+
+export default RootLayout
+
+const styles = StyleSheet.create({})
